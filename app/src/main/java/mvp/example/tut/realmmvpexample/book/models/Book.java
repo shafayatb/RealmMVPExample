1850399e106c62b7bef3d098185d4716828ec28f
shafayatb <mamunshafayatbin@yahoo.com>
2016-07-27 16:57:21 +0600

    commit

--- app/src/main/java/mvp/example/tut/realmmvpexample/book/models/Book.java
+++ app/src/main/java/mvp/example/tut/realmmvpexample/book/models/Book.java
@@ -1,7 +1,57 @@
 package mvp.example.tut.realmmvpexample.book.models;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by iBaax on 7/27/16.
  */
-public class Book {
+public class Book extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+    private String title;
+    private String description;
+    private String author;
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
